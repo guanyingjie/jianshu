902d5f98b3d5608659482697e81206d119c68f12
赵文坤 <wenkun.zhao@renren-inc.com>
2018-12-16 15:41:38 +0800

    第一次提交

---
+++ src/common/header/index.js
@@ -0,0 +1,36 @@
+import React,{Component} from 'react';
+import {
+    HeaderWrapper,
+    Logo,
+    Nav,
+    NavItem,
+    NavSearch,
+    Addition,
+    Button
+} from "./style";
+
+
+
+class  Header extends Component{
+    render(){
+        return(
+                <HeaderWrapper>
+                    <Logo />
+                    <Nav>
+                        <NavItem className='left' active>首页</NavItem>
+                        <NavItem className='left'>下载APP</NavItem>
+                        <NavItem className='right'>登录</NavItem>
+                        <NavItem className='right'>Aa</NavItem>
+                        <NavSearch></NavSearch>
+                    </Nav>
+                    <Addition>
+                        <Button className='writting'>写文章</Button>
+                        <Button className='reg'>注册</Button>
+
+                    </Addition>
+                </HeaderWrapper>
+            )
+    }
+
+}
+export default Header;//导出组件便于在其他地方引用
